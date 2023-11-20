08cc0c379a4d856faac7b528cc4567762bfc69f3
Pavlo <pasam9300@gmail.com>
2023-11-20 17:36:51 +0100

    update

--- js/languages.js
+++ js/languages.js
@@ -2,7 +2,6 @@ document.querySelector('.dropbtn').addEventListener('click', function () {
   document.querySelector('.dropdown-content').classList.toggle('show');
 });
 
-// Закривати випадаюче меню при кліку поза його межами
 window.onclick = function (event) {
   if (!event.target.matches('.dropbtn')) {
     var dropdowns = document.getElementsByClassName('dropdown-content');

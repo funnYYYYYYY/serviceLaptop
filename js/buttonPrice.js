abf56938179fef269904a641930566bd53bb8e3b
Pavlo <pasam9300@gmail.com>
2023-11-21 19:42:23 +0100

    update

--- js/buttonPrice.js
+++ js/buttonPrice.js
@@ -1,6 +1,4 @@
-// Функція для показу цін та встановлення класу active
 function showPrices(category, clickedButton) {
-  // Приховуємо всі таблиці та видаляємо клас active з усіх кнопок
   const allPrices = document.querySelectorAll('.prices');
   const allButtons = document.querySelectorAll('.price__btn');
   allPrices.forEach(price => {
@@ -10,17 +8,16 @@ function showPrices(category, clickedButton) {
     button.classList.remove('active');
   });
 
-  // Показуємо вибрану таблицю
   const selectedCategory = document.querySelector(`.${category}`);
   if (selectedCategory) {
     selectedCategory.classList.remove('visually-hidden');
   }
 
-  // Додаємо клас active до натиснутої кнопки
-  clickedButton.classList.add('active');
+  if (clickedButton) {
+    clickedButton.classList.add('active');
+  }
 }
 
-// Додаємо обробник подій до кожної кнопки
 document.addEventListener('DOMContentLoaded', () => {
   const buttons = document.querySelectorAll('.price__btn');
   buttons.forEach(button => {
@@ -29,4 +26,11 @@ document.addEventListener('DOMContentLoaded', () => {
       showPrices(category, this);
     });
   });
+
+  // Активувати кнопку "Комп'ютер" за замовчуванням
+  const defaultCategory = 'laptop';
+  const defaultButton = document.querySelector(`.price__btn[data-category="${defaultCategory}"]`);
+  if (defaultButton) {
+    showPrices(defaultCategory, defaultButton);
+  }
 });

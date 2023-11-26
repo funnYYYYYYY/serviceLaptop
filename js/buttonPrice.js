330618bf898d152f623792c4aa42944b634e535b
Pavlo <pasam9300@gmail.com>
2023-11-26 16:59:32 +0100

    Update

--- js/buttonPrice.js
+++ js/buttonPrice.js
@@ -1,20 +1,29 @@
 function showPrices(category, clickedButton) {
-  const allPrices = document.querySelectorAll('.prices');
-  const allButtons = document.querySelectorAll('.price__btn');
-  allPrices.forEach(price => {
-    price.classList.add('visually-hidden');
-  });
-  allButtons.forEach(button => {
-    button.classList.remove('active');
-  });
+  if (window.innerWidth <= 1204) {
+    // Показати всі таблиці цін на ширині вікна до 1204px
+    const allPrices = document.querySelectorAll('.prices');
+    allPrices.forEach(price => {
+      price.classList.remove('visually-hidden');
+    });
+  } else {
+    // Застосувати логіку приховування та показу як зазвичай
+    const allPrices = document.querySelectorAll('.prices');
+    const allButtons = document.querySelectorAll('.price__btn');
+    allPrices.forEach(price => {
+      price.classList.add('visually-hidden');
+    });
+    allButtons.forEach(button => {
+      button.classList.remove('active');
+    });
 
-  const selectedCategory = document.querySelector(`.${category}`);
-  if (selectedCategory) {
-    selectedCategory.classList.remove('visually-hidden');
-  }
+    const selectedCategory = document.querySelector(`.${category}`);
+    if (selectedCategory) {
+      selectedCategory.classList.remove('visually-hidden');
+    }
 
-  if (clickedButton) {
-    clickedButton.classList.add('active');
+    if (clickedButton) {
+      clickedButton.classList.add('active');
+    }
   }
 }
 
@@ -27,10 +36,18 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 
-  // Активувати кнопку "Комп'ютер" за замовчуванням
-  const defaultCategory = 'laptop';
-  const defaultButton = document.querySelector(`.price__btn[data-category="${defaultCategory}"]`);
-  if (defaultButton) {
-    showPrices(defaultCategory, defaultButton);
+  // Активувати відповідний стан в залежності від ширини вікна
+  if (window.innerWidth <= 1204) {
+    const allPrices = document.querySelectorAll('.prices');
+    allPrices.forEach(price => {
+      price.classList.remove('visually-hidden');
+    });
+  } else {
+    // Активувати кнопку "laptop" за замовчуванням
+    const defaultCategory = 'laptop';
+    const defaultButton = document.querySelector(`.price__btn[data-category="${defaultCategory}"]`);
+    if (defaultButton) {
+      showPrices(defaultCategory, defaultButton);
+    }
   }
 });
